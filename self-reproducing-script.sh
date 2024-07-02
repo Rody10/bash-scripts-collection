07a49d5e7f4d093022ed67a3e5db6b7dec277cc5
Rody10 <36506046+Rody10@users.noreply.github.com>
2024-07-02 11:15:37 +0200

    Create self-reproducing-script.sh

---
+++ self-reproducing-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# script #!/bin/bash
+
+# script that backs itself up to a file called backup.sh to a file called backup.sh
+
+# append contents of this script to backup.sh
+# if backup.sh does not exist, it will be created automatically
+cat $0 | cat >> backup.sh
